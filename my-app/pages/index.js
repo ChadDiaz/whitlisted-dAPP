e6badcbd0b6306a7c9a49917cb1b9ecb1e65ad6a
ChadDiaz <chadcdiaz@gmail.com>
2022-09-04 16:48:10 -0400

    testing chainId

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -40,7 +40,7 @@ export default function Home() {
 
     // if user is not connected to the goerli network, let them know and throw an error//
     const { chainId } = await web3Provider.getNetwork();
-    if (chainId !== 5) {
+    if (chainId !== 3) {
       window.alert('Change the network to Goerli Test Network');
       throw new Error('Change network to Goerli Test Network');
     }
